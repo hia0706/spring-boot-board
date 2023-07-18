256306262c9fb17b142c52787d9273e943206e65
ljjs0121 <ljjs1215@gmail.com>
2023-07-18 15:28:59 +0900

    Feat: 회원가입 기능 추가

--- src/main/java/kr/co/jhta/service/UserService.java
+++ src/main/java/kr/co/jhta/service/UserService.java
@@ -1,5 +1,10 @@
 package kr.co.jhta.service;
 
+import kr.co.jhta.vo.UserRole;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.co.jhta.dao.UserDao;
@@ -7,16 +12,53 @@ import kr.co.jhta.dao.UserRoleDao;
 import kr.co.jhta.vo.User;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private final UserDao userDao;
 	private final UserRoleDao userRoleDao;
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userDao.getUserByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("["+email+"]");
+		}
+
+		List<UserRole> userRoles = userRoleDao.getUserRolesByUserNo(user.getNo());
+		List<String> roleNames = new ArrayList<>();
+		for (UserRole role : userRoles) {
+			roleNames.add(role.getRoleName());
+		}
+		user.setRoleNames(roleNames);
+
+		return user;
+	}
 	
 	// 신규 사용자 등록하기
-	public void createUser(User user) {
-		
+	public void createUser(String email, String password) {
+		User user = new User();
+		user.setEmail(email);
+		user.setEncryptedPassword(password);
+
+		// 1. 비밀번호를 암호화해서 저장시키기
+		String encryptedPassword = passwordEncoder.encode(user.getPassword());
+		user.setEncryptedPassword(encryptedPassword);
+
+		// 2. 테이블에 사용자 정보 저장하기
+		userDao.insertUser(user);
+
+		// 3. 테이블에 사용자 보유권한 저장하기
+		UserRole role = new UserRole();
+		role.setUser(user);
+		role.setRoleName("ROLE_USER");
+
+		userRoleDao.insertUserRole(role);
 	}
 	
 	// 사용자 인증하기
--- src/main/java/kr/co/jhta/web/controller/UserController.java
+++ src/main/java/kr/co/jhta/web/controller/UserController.java
@@ -1,6 +1,8 @@
 package kr.co.jhta.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.jhta.service.UserService;
@@ -12,16 +14,27 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UserService userService;
-	
+
 	// 사용자 등록화면 요청과 매핑되는 요청핸들러 메소드
-	
+	@GetMapping("/register")
+	public String form() {
+
+		return "/user/registerform";
+	}
+
 	// 사용자 등록 요청과 매핑되는 요청핸들러 메소드
-	
+	@PostMapping("/register")
+	public String register(String email, String password) {
+		userService.createUser(email, password);
+
+		return "redirect:/";
+	}
+
 	// 로그인 화면 요청과 매핑되는  요청핸들러 메소드
-	
+
 	// 내정보보기 화면 요청과 매핑되는 요청핸들러 메소드
-	
+
 	// 내정보수정화면 요청과 매핑되는 요청핸들러 메소드
-	
+
 	// 내정보 수정 요청과 매팅되는 요청핸들러 메소드
 }
